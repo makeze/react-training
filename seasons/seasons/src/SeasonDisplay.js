cc2d012d43a49bffb487fa29a3e15206ffea6c0e
makeze <max.gana93@gmail.com>
2020-09-24 23:07:44 +0200

    pretty code

--- seasons/seasons/src/SeasonDisplay.js
+++ seasons/seasons/src/SeasonDisplay.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
+const seasonText = {
+    summer: {
+        text: "Lets swim!",
+        icon: "sun"
+    },
+    winter: {
+        text: "So cold outside.",
+        icon: "snowflake"
+    }
+};
+
 const SeasonDisplay = props => {
-    console.log(props.lat);
     const season = getSeason(props.lat, new Date().getMonth());
-    return <div>Season Display: {props.lat}, {season}</div>
-}
+    const {text, icon} = seasonText[season];
+    return (
+        <div>
+            {text}: {props.lat} <i className={icon + " icon"}/>
+        </div>
+    );
+};
 
 const getSeason = (lat, month) => {
     if(month > 2 && month < 9){
         return lat > 0 ? 'summer' : 'winter';
     }
- }
+ };
 
 export default SeasonDisplay;
