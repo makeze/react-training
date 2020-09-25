d3669275936c2440ffa4ec44c07ce8000cadbb03
Maksik <max.gana93@gmail.com>
2020-09-25 21:06:21 +0200

    loading screen fix

--- seasons/seasons/src/LoadingScreen.js
+++ seasons/seasons/src/LoadingScreen.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 const LoadingScreen = () => {
     return (
-        <div className="ui segment season-display">
-            <div className="ui active dimmer">
-                <div className="ui text loader">Loading</div>
-            </div>
-            <p></p>
+        <div className="ui active dimmer">
+            <div className="ui text loader">Loading</div>
         </div>
     )
 }
