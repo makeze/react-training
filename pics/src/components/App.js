0d29f20f3a5b44056c2fdaae05eee27262b28732
Maksik <max.gana93@gmail.com>
2020-09-27 23:26:06 +0200

    async api request

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    onSearchSubmit = (searchTerm) => {
-        console.log(this.constructor.name, searchTerm);
-        axios.get('https://api.unsplash.com/photos/', {
+    onSearchSubmit = async (searchTerm) => {
+        const response = await axios.get('https://api.unsplash.com/photos/', {
             params: {query: searchTerm},
             headers: {
                 Authorization: 'Client-ID kW3cEPCLiahyXDGLjZLPVVFpn5S78GpoWx48-a0fJjo'
