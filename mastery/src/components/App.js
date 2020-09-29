9525841fee0b148b5006555fdcca99fe0ca13ece
makeze <max.gana93@gmail.com>
2020-09-30 01:56:00 +0200

    finished mastery

--- mastery/src/components/App.js
+++ mastery/src/components/App.js
@@ -11,6 +11,10 @@ class App extends React.Component {
         selectedVideo: null
     };
 
+    componentDidMount() {
+        this.onSubmit('cookies');
+    }
+
     onSubmit = async (searchTerm) => {
 
         const response = await youtube.get('search', {
@@ -19,7 +23,8 @@ class App extends React.Component {
             }
         });
         this.setState({
-            videos: response.data.items
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
         });
     };
 
@@ -33,8 +38,16 @@ class App extends React.Component {
         return(
             <div className="ui container">
                 <SearchBar onSubmit={this.onSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- mastery/src/components/VideoDetail.js
+++ mastery/src/components/VideoDetail.js
@@ -6,8 +6,12 @@ const VideoDetail = ({video}) => {
             <div>Loading...</div>
         )
     }
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
     return (
         <div>
+            <div className="ui embed">
+                <iframe title="video-player" src={videoSrc}/>
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
                 <p>{video.snippet.description}</p>
