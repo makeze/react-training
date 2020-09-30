d1cb03e45c3bfecc6c749ba169e689c5f14b8563
makeze <max.gana93@gmail.com>
2020-10-01 00:16:30 +0200

    hooknhooks

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Accordion = ({items}) => {
-    return <h1>I have {items.length} items</h1>;
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const onTitleClick = (index) => {
+        console.log('Title clicked', index);
+        setActiveIndex(index);
+    };
+
+    const renderedItems = items.map((item, index) => {
+        return (
+            <React.Fragment key={item.title}>
+                <div className="title active" onClick={() => onTitleClick(index)}>
+                    <i className="dropdown icon"/>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    {item.origin}
+                </div>
+            </React.Fragment>
+        )});
+
+    return <div className="ui styled accordion">
+        {renderedItems}
+        <h1>{activeIndex}</h1>
+    </div>;
 };
 
 export default Accordion;
