8070e5a13380b2d4e18c43e9e55516b33b8dd494
makeze <max.gana93@gmail.com>
2020-09-30 12:24:45 +0200

    basic data

---
+++ widgets/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Accordion from "./components/Accordion";
+
+const items = [
+    {
+        title: "maksudik",
+        origin: "russian republic, dagestan, kaspiysk"
+    },
+    {
+        title: "roma",
+        origin: "republic of belarus, minsk"
+    },
+    {
+        title: "artem",
+        origin: "republic of belarus, minsk"
+    },
+    {
+        title: "sergei",
+        origin: "lithuania, ignalinos rajonas, visaginas"
+    }
+];
+
+export default () => {
+    return (
+        <Accordion items={items} />
+    )
+};
---
+++ widgets/src/components/Accordion.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Accordion = ({items}) => {
+    return <h1>I have {items.length} items</h1>;
+};
+
+export default Accordion;
