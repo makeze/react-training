f6e9974e2069a66b351b67203ab786dc03f062ba
Maksik <max.gana93@gmail.com>
2020-10-10 16:12:53 +0200

    wip

--- streaming/client/src/actions/index.js
+++ streaming/client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import {SIGN_IN, SIGN_OUT} from "./types";
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from "./types";
 
 export const signIn = (userId) => {
     return {
@@ -13,3 +21,28 @@ export const signOut = () => {
         type: SIGN_OUT
     }
 };
+
+export const createStream = formValues => async dispatch => {
+    const response = streams.post('/streams', formValues);
+    dispatch({type: CREATE_STREAM, payload: response.data});
+}
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({ type: FETCH_STREAMS, payload: response.data});
+}
+
+export const fetchStream = (streamId) => async dispatch => {
+    const response = await streams.get(`/streams/${streamId}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data});
+}
+
+export const deleteStream = (streamId) => async dispatch => {
+    const response = await streams.delete(`/streams/${streamId}`);
+    dispatch({ type: DELETE_STREAM});
+}
+
+export const editStream = (streamId, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${streamId}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data});
+}
--- streaming/client/src/index.js
+++ streaming/client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
     <Provider store={store}>
