b184957b3db34ea6b74faf2f5dd6a8b38a560e98
makeze <max.gana93@gmail.com>
2020-10-15 16:09:28 +0200

    context fun

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
     state = {language: 'english'};
@@ -16,7 +18,11 @@ class App extends React.Component {
                     <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
                     <i className="flag ru" onClick={() => this.onLanguageChange('russian')}/>
                 </div>
-                <UserCreate/>
+                <LanguageContext.Provider value={this.state.language}>
+                    <ColorContext.Provider value="red">
+                        <UserCreate/>
+                    </ColorContext.Provider>
+                </LanguageContext.Provider>
             </div>
         )
     }
--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
+    static contextType = LanguageContext;
+
     render() {
         return (
-            <button className="ui primary">Submit</button>
+            <ColorContext.Consumer>
+                {(color) =>
+                    <button className={`ui button ${color}`}>
+                        <LanguageContext.Consumer>
+                            {(value) => value === 'eтglish' ? 'Submit' : 'Войти'}
+                        </LanguageContext.Consumer>
+                    </button>
+                }
+            </ColorContext.Consumer>
         )
     }
 }
+
+Button.contextType = LanguageContext;
 
 export default Button;
